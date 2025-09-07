90d160d590bc71b1d083de08480f176350c061ff
dweaverjr <80757650+dweaverjr@users.noreply.github.com>
2025-09-07 09:26:26 -0400

    Implement SystemBus class for memory and I/O interconnect

---
+++ include/core/bus.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "core/component.hpp"
+#include "core/types.hpp"
+#include <memory>
+
+namespace nes {
+
+// Forward declarations
+class Ram;
+
+/// System Bus - Central memory and I/O interconnect
+/// Handles address decoding and routes memory accesses to appropriate components
+class SystemBus final : public Component {
+  public:
+	SystemBus();
+	~SystemBus() = default;
+
+	// Component interface
+	void tick(CpuCycle cycles) override;
+	void reset() override;
+	void power_on() override;
+	[[nodiscard]] const char *get_name() const noexcept override;
+
+	// Memory interface
+	[[nodiscard]] Byte read(Address address) const;
+	void write(Address address, Byte value);
+
+	// Component management
+	void connect_ram(std::shared_ptr<Ram> ram);
+	// TODO: Add other component connections as we implement them
+	// void connect_ppu(std::shared_ptr<PPU> ppu);
+	// void connect_apu(std::shared_ptr<APU> apu);
+
+	// Debug interface
+	void debug_print_memory_map() const;
+
+  private:
+	// Connected components
+	std::shared_ptr<Ram> ram_;
+	// std::shared_ptr<PPU> ppu_;  // TODO: Add when implemented
+	// std::shared_ptr<APU> apu_;  // TODO: Add when implemented
+
+	// Address decoding helpers
+	[[nodiscard]] bool is_ram_address(Address address) const noexcept;
+	[[nodiscard]] bool is_ppu_address(Address address) const noexcept;
+	[[nodiscard]] bool is_apu_address(Address address) const noexcept;
+
+	// Open bus simulation
+	mutable Byte last_bus_value_ = 0xFF;
+};
+
+} // namespace nes
